72e7cfc2d27335511db0b8d635a095f2da8bed7b
multileo-ai <kumar.ashutosh.er@gmail.com>
2025-05-28 18:24:54 +0530

    EWf

--- src/Components/EditProfileForm.jsx
+++ src/Components/EditProfileForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const EditProfileForm = () => {
+const EditProfileForm = ({ onEditClick }) => {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [bio, setBio] = useState("");
